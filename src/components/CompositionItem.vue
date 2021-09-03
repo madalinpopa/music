27aff8a2a1a6d69e0d2c778a1de78a7e93913174
Madalin Popa <popa.madalin@live.com>
2021-09-03 09:02:54 +0300

    add validation message for edit song form

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -3,16 +3,7 @@
     <div v-show="!showForm">
       <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
       <button
-        class="
-          ml-1
-          py-1
-          px-2
-          text-sm
-          rounded
-          text-white
-          bg-red-600
-          float-right
-        "
+        class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
       >
         <i class="fa fa-times"></i>
       </button>
@@ -33,11 +24,16 @@
       </button>
     </div>
     <div v-show="showForm">
-      <form>
+      <vee-form
+        :validation-schema="schema"
+        :initial-values="song"
+        @submit="edit"
+      >
         <div class="mb-3">
           <label class="inline-block mb-2">Song Title</label>
-          <input
+          <vee-field
             type="text"
+            name="modified_name"
             class="
               block
               w-full
@@ -47,17 +43,18 @@
               border border-gray-300
               transition
               duration-500
-              focus:outline-none
-              focus:border-black
+              focus:outline-none focus:border-black
               rounded
             "
             placeholder="Enter Song Title"
           />
+          <ErrorMessage class="text-red-600" name="modified_name" />
         </div>
         <div class="mb-3">
           <label class="inline-block mb-2">Genre</label>
-          <input
+          <vee-field
             type="text"
+            name="genre"
             class="
               block
               w-full
@@ -67,12 +64,12 @@
               border border-gray-300
               transition
               duration-500
-              focus:outline-none
-              focus:border-black
+              focus:outline-none focus:border-black
               rounded
             "
             placeholder="Enter Genre"
           />
+          <ErrorMessage class="text-red-600" name="genre" />
         </div>
         <button
           type="submit"
@@ -86,7 +83,7 @@
         >
           Go Back
         </button>
-      </form>
+      </vee-form>
     </div>
   </div>
 </template>
@@ -102,7 +99,16 @@ export default {
   data() {
     return {
       showForm: false,
+      schema: {
+        modified_name: 'required',
+        genre: 'alpha_spaces',
+      },
     };
+  },
+  methods: {
+    edit() {
+      console.log('Song edited');
+    },
   },
 };
 </script>
